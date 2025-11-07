2fea8af121d6c25f15b45e7742fbbdaedfb76a57
fpfcmsr <fpf@andrew.cmu.edu>
2025-11-07 04:12:49 +0000

    add zfs manager stuff and package add

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,15 +11,21 @@ apt update
 
 # zfs stuff
 apt install -y linux-image-amd64 linux-headers-amd64
-apt install -y zfs-initramfs zfsutils-linux zfs-dkms cryptsetup cryptsetup-initramfs
+apt install -y zfs-dkms zfs-zed zfs-initramfs zfsutils-linux 
 
-# systemdboot stuff
+# systemdboot + luks stuff 
+apt install -y cryptsetup cryptsetup-initramfs
 apt install -y efitools sbsigntool efibootmgr openssl systemd-ukify rsync systemd-boot
 
 #podman stuff
-apt install -y podman 
+apt install -y podman
+
+# ssh
+openssh-server
 
 #cockpit stuff
-apt install -y cockpit cockpit-machines
+apt install -y cockpit cockpit-bridge cockpit-machines cockpit-networkmanager cockpit-packagekit cockpit-podman cockpit-storaged cockpit-system cockpit-ws
 # note to add the zfs manager stuff also 
-
+mkdir /tmp/cockpit-zfs-manager
+git clone https://github.com/45drives/cockpit-zfs-manager.git /tmp/cockpit-zfs-manager
+sudo cp -r /tmp/cockpit-zfs-manager/zfs /usr/share/cockpit
