bf381074ba465ecf25aa381b8ad40863a674abf8
fpfcmsr <fpf@andrew.cmu.edu>
2025-12-06 01:36:59 +0000

    remove docjer

--- build_files/build.sh
+++ build_files/build.sh
@@ -20,18 +20,6 @@ apt install -y efitools sbsigntool efibootmgr openssl systemd-ukify rsync system
 
 #container stuff
 apt install -y podman
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-tee /etc/apt/sources.list.d/docker.sources <<EOF
-Types: deb
-URIs: https://download.docker.com/linux/debian
-Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
-Components: stable
-Signed-By: /etc/apt/keyrings/docker.asc
-EOF
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # ssh
 apt install -y openssh-server
